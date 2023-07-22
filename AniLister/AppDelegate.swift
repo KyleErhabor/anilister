5686d42686135a5939fa3c23339289cb5de06398
Kyle Erhabor <kyleerhabor@gmail.com>
2023-07-21 21:24:40 -0400

    Initial Commit

---
+++ AniLister/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  AniLister
+//
+//  Created by Kyle Erhabor on 7/21/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
